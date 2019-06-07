c0d2d30664aa7c629db9b5a871ea569f7225b051
gang <957001352@qq.com>
2019-06-07 09:48:27 +0800

    架构搭建

---
+++ src/main/java/com/woniu/web/action/UserAction.java
@@ -0,0 +1,10 @@
+package com.woniu.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport{
+	public String save() {
+		System.out.println("save");
+		return null;
+	}
+}
---
+++ src/test/java/com/woniu/test/AppTest.java
@@ -0,0 +1,12 @@
+package com.woniu.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void test1() throws Exception {
+		
+	}
+}
